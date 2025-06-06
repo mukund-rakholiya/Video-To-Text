844167bc1f5718c08a074300f98bb754c395d403
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-06-06 17:35:35 +0530

    deepgram ai working properly but model and api key issues

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import asyncio
-from pathlib import Path
 # DEEPGRAM: Commenting out Deepgram import
 # from transcribers import transcribe_audio, transcribe_audio_deepgram
+import asyncio
+from pathlib import Path
 from transcribers import transcribe_audio
 from utils import extract_audio_from_video
 import os
